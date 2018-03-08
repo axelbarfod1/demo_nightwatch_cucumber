94970516726ff193f71c23c31838e3195ac2b771
Axel Barfod <axel.barfod@mercadolibre.com>
2018-03-08 14:07:40 -0300

    Agrego otro conf

---
+++ nightwatch.conf.BASIC.js
@@ -0,0 +1,59 @@
+const seleniumServer = require('selenium-server')
+const phantomjs = require('phantomjs-prebuilt')
+const chromedriver = require('chromedriver')
+
+require('nightwatch-cucumber')({
+  cucumberArgs: ['--require', 'features/step_definitions', '--format', 'json:reports/cucumber.json', 'features']
+})
+
+module.exports = {
+  output_folder: 'reports',
+  custom_assertions_path: '',
+  page_objects_path: 'page_objects',
+  live_output: false,
+  disable_colors: false,
+  test_workers: {
+    enabled: false,
+    workers: 2
+  },
+  selenium: {
+    start_process: true,
+    server_path: seleniumServer.path,
+    log_path: '',
+    host: '127.0.0.1',
+    port: 4444
+  },
+  test_settings: {
+    default: {
+      launch_url: 'http://www.mercadolibre.com.ar',
+      selenium_port: 4444,
+      selenium_host: 'localhost',
+      screenshots: {
+        enabled: true,
+        on_failure: true,
+        on_error: true,
+        path: "./reports"
+    },
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['--incognito', 'disable-extensions', '--no-sandbox']
+        },
+        selenium: {
+          cli_args: {
+            'webdriver.chrome.driver': chromedriver.path
+          }
+        }
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true,
+        acceptSslCerts: true
+      }
+    }
+  }
+};
